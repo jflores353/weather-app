c6f7881ce4ede1193d80b5fdcb7b68aa93230d5d
Gabe <jflores353@gmail.com>
2019-05-03 12:37:09 -0500

    created Header & Form Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
+import Form from './Components/Form';
 
 function App() {
   return (
     <div className="App">
-     <h1>Weather App</h1>
+     <Header />
+     <Form />
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to your Local Weather App</h1>
+        <p>Search to display your temperature, weather conditions and more ...</p>
+      </div>
+    )
+  }
+}
+
+export default Header;
