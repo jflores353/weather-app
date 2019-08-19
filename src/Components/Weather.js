75fd5fc2d50ada470f9241ff6ca3d23fbbbd6045
Gabe <jflores353@gmail.com>
2019-08-19 07:35:09 -0500

    created if statements with the && operator to display elements only when returned true

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 
 class Weather extends Component {
   render() {
-    return <div>{this.props.temperature}</div>;
+    return (
+      <div>
+        {this.props.city && this.props.country && (<p>Location:{this.props.city},{this.props.country}</p>)}
+        {this.props.temperature && <p>Temperature:{this.props.temperature}</p>}
+        {this.props.description && <p>Condition:{this.props.description}</p>}
+      </div>
+    );
   }
 }
 
