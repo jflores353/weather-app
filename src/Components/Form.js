60b47467c347d21e83b65f6e9fe5c8a5a6e6e4e1
Gabe <jflores353@gmail.com>
2019-08-18 21:37:56 -0500

    created form for getting weather

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Form = () => {
   return (
     <div>
-      Form Component
+      <input type="text" name="city" placeholder="Enter City Name ..." />
+      <input type="text" name="country" placeholder="Enter Country ..." />
+      <button>Get Weather</button>
     </div>
-  )
-}
+  );
+};
 
 export default Form;
