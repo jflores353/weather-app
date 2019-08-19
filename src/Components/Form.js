8a753355e7227b9a98c38740e592b9409d781e0c
Gabe <jflores353@gmail.com>
2019-08-18 22:25:47 -0500

    passed prop of getWeather to form onSubmit.

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-const Form = () => {
-  return (
-    <div>
-      <input type="text" name="city" placeholder="Enter City Name ..." />
-      <input type="text" name="country" placeholder="Enter Country ..." />
-      <button>Get Weather</button>
-    </div>
-  );
-};
+class Form extends React.Component {
+  render() {
+    return (
+      <form onSubmit={this.props.getWeather}>
+        <input type="text" name="city" placeholder="Enter City ..." />
+        <input type="text" name="country" placeholder="Enter Country ..." />
+        <button>Get Weather</button>
+      </form>
+    );
+  }
+}
 
 export default Form;
