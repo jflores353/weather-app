3ee575463c0a69dcab14ef747f4230aba2eba7dc
Gabe <jflores353@gmail.com>
2019-08-19 16:38:22 -0500

    Gave classNames to key/values

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -3,10 +3,21 @@ import React from "react";
 const Weather = props => {
     return (
       <div className="weather-content">
-        { props.city && props.country && (<p>Location:{ props.city },{ props.country }</p>) }
-        { props.temperature && <p>Temperature:{ props.temperature }</p> }
-        { props.description && <p>Condition:{ props.description }</p> }
-        { props.error && <p>{ props.error }</p> }
+        { 
+          props.city && props.country && <p className="weather-key">Location:
+          <span className="weather-value"> { props.city },{ props.country }</span></p> 
+        }
+        { 
+          props.temperature && <p className="weather-key">Temperature:
+          <span className="weather-value"> { props.temperature }</span></p> 
+        }
+        { 
+          props.description && <p className="weather-key">Condition:
+          <span className="weather-value"> { props.description }</span></p> 
+        }
+        { 
+          props.error && <p className="weather-error"> { props.error }</p> 
+        }
       </div>
     );
 }
