3dece6bec2794c7332d3ead1906b5cad6c612074
Gabe <jflores353@gmail.com>
2019-08-18 21:28:05 -0500

    made getWeather() to fetch from weather API, will have to link API_KEY

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import './App.css';
-import Header from './Components/Header';
-import Form from './Components/Form';
+import React from "react";
+import Header from "./Components/Header";
+import Form from "./Components/Form";
+import Weather from "./Components/Weather";
 
-function App() {
-  return (
-    <div className="App">
-     <Header />
-     <Form />
-    </div>
-  );
+const API_KEY = "6fb54323d6d6e0e48df0c278f715f04e";
+
+export default class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`
+    );
+    const data = await api_call.json();
+    console.log(data);
+  };
+  render() {
+    return (
+      <div>
+        <Header />,
+        <Form />,
+        <Weather />
+      </div>
+    );
+  }
 }
-
-export default App;
