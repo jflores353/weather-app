3b22bea38c2c089f4279cdad827593a84e5cf02f
Gabe <jflores353@gmail.com>
2019-08-18 22:45:54 -0500

    created State in App, after api_call setState with given city and country

--- src/App.js
+++ src/App.js
@@ -6,15 +6,28 @@ import Weather from "./Components/Weather";
 const API_KEY = "6fb54323d6d6e0e48df0c278f715f04e";
 
 export default class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    description: undefined,
+    error: undefined
+  };
   getWeather = async e => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     const api_call = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weathe{r?q=${city},${country}&appid=${API_KEY}`
     );
     const data = await api_call.json();
     console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      description: data.weather[0].description
+    });
   };
   render() {
     return (
