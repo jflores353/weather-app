cca8ee897f4a737b8571662dde73492f1687d0ea
Gabe <jflores353@gmail.com>
2019-08-19 10:05:24 -0500

    added className weather-content

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Weather = props => {
     return (
-      <div>
+      <div className="weather-content">
         { props.city && props.country && (<p>Location:{ props.city },{ props.country }</p>) }
         { props.temperature && <p>Temperature:{ props.temperature }</p> }
         { props.description && <p>Condition:{ props.description }</p> }
