ce621a9bb29175b7097e5ca68aef1faca64fec7e
Gabe <jflores353@gmail.com>
2019-08-18 22:22:16 -0500

    Passed prop of getWeather to Form; created variables for city and country

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import Weather from "./Components/Weather";
 const API_KEY = "6fb54323d6d6e0e48df0c278f715f04e";
 
 export default class App extends React.Component {
-  getWeather = async () => {
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
     const api_call = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
     );
     const data = await api_call.json();
     console.log(data);
@@ -17,7 +20,7 @@ export default class App extends React.Component {
     return (
       <div>
         <Header />,
-        <Form />,
+        <Form getWeather={this.getWeather} />,
         <Weather />
       </div>
     );
